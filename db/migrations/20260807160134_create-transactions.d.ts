03e38550f45d50814d22758d1fe89aac042fd12a
Nicolas Bezerra <nicolasbezerra13@gmail.com>
2026-09-18 18:03:39 +0100

    refactor: remove obsolete transaction migration files

--- db/migrations/20260807160134_create-transactions.d.ts
+++
@@ -1,4 +0,0 @@
-import type { Knex } from "knex";
-export declare function up(knex: Knex): Promise<void>;
-export declare function down(knex: Knex): Promise<void>;
-//# sourceMappingURL=20260807160134_create-transactions.d.ts.map
